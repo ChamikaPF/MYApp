4d34679e586bef4a08a0eb5c8eb4a108f93a3fc4
ChamikaPF <40482275+ChamikaPF@users.noreply.github.com>
2018-07-03 21:26:04 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var x=prompt("Enter thr number");
+var squrt=Math.sqrt(x);
+document.write("square root  is "+squrt);
+
+var y=prompt("Enter the radius");
+var p=Math.PI;
+var x=p*y*y;
+document.write("<br>cycle area is "+x);
+
---
+++ jsvalidation.js
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+
+ <script language="javascript" type="text/javascript">
+function validateform()
+{
+	if(document.form.startdate.value.length == 0)
+	{
+		alert("Please enter date");
+		return;
+	}	
+	if(document.form.starttime.value.length ==0)
+	{
+		alert("Please enter time");
+		return;
+	}
+}
+</script>
+</head>
+
+<body>
+
